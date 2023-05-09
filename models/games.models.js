53b6ba85384dd4e2df31e2f16dc872ebfe699705
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2023-05-09 15:01:57 +0100

    took out comments from models

--- models/games.models.js
+++ models/games.models.js
@@ -5,12 +5,3 @@ return connection.query("SELECT * FROM categories;").then((result) => {
     return result.rows
 })
 }
-
-// Responds with:
-
-// an array of category objects, each of which should have the following properties:
-// slug
-// description
-// As this is the first endpoint you will need to set up your testing suite.
-
-// Errors handled.
