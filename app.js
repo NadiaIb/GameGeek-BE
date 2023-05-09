48c5b51c9372568162ca25ef1cb44263fba7af2a
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2023-05-09 17:03:14 +0100

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
-const { getCategories, getReviews } = require("./controllers/games.controllers");
+const {
+  getCategories,
+  getReviews,
+} = require("./controllers/games.controllers");
+
+app.get("/api", (req, res)=>{
+    fs.readFile("/endpoints.json", "utf8", function (err, data){
+        if (err) throw err;   console.log(data); 
+    })
+})
 
 app.get("/api/categories", getCategories);
 
